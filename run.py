68ede1e515fb1ea430e669f0bb7e9d03097279af
fieryhenry <github@henrythomas.me.uk>
2024-01-30 19:15:48 +0000

    updated for new structure

--- run.py
+++ run.py
@@ -1,22 +1,22 @@
-from tbcml import core
+import tbcml
 
-# https://github.com/fieryhenry/TBCModLoader
-# currently working as of commit 18dcd32
+# https://github.com/fieryhenry/tbcml/tree/new-structure
+# currently working as of commit https://github.com/fieryhenry/tbcml/commit/e716282adcbed53c1bac034fe7a5d66ff396e1c4
 
 
-def do(cc: core.CountryCode):
+def do(cc: tbcml.CountryCode):
     print(cc.get_code())
-    gv = core.GameVersion.from_string_latest("latest", cc)
+    gv = tbcml.GameVersion.from_string_latest("latest", cc)
     print(gv.to_string())
 
-    apk = core.Apk(gv, cc)
+    apk = tbcml.Apk(gv, cc)
     success = apk.download()  # progress=None)
     if not success:
         print("Failed to download apk")
         return
     apk.extract()
 
-    game_data = core.GamePacks.from_apk(apk)
+    game_data = tbcml.GamePacks.from_apk(apk)
 
     packnames = [
         "DataLocal",
@@ -28,7 +28,7 @@ def do(cc: core.CountryCode):
         "resLocal_th",
     ]
 
-    folder = core.Path(f"{gv.to_string()}{cc.get_code()}")
+    folder = tbcml.Path(f"{gv.to_string()}{cc.get_code()}")
 
     for packname in packnames:
         pack = game_data.get_pack(packname)
@@ -36,54 +36,54 @@ def do(cc: core.CountryCode):
             continue
         pack.extract(folder)
 
-    server_path = core.Path(f"{cc.get_code()}_server")
+    server_path = tbcml.Path(f"{cc.get_code()}_server")
 
     apk.download_server_files()
-    apk.get_server_path(apk.country_code).copy_tree(server_path)
+    apk.get_server_path().copy_tree(server_path)
 
     delete_old(cc)
     update_latest_txt(cc)
 
 
-def delete_old(cc: core.CountryCode):
-    all_paths: list[core.Path] = []
-    for folder in core.Path(".").get_dirs():
+def delete_old(cc: tbcml.CountryCode):
+    all_paths: list[tbcml.Path] = []
+    for folder in tbcml.Path(".").get_dirs():
         if folder.get_file_name().endswith(cc.get_code()):
             all_paths.append(folder)
 
-    latest_version = core.GameVersion.from_string_latest("latest", cc)
+    latest_version = tbcml.GameVersion.from_string_latest("latest", cc)
     for path in all_paths:
         if path.get_file_name() == f"{latest_version.to_string()}{cc.get_code()}":
             continue
         path.remove()
 
 
-def update_latest_txt(cc: core.CountryCode):
-    latest_version = core.GameVersion.from_string_latest("latest", cc)
-    path = core.Path("latest.txt")
+def update_latest_txt(cc: tbcml.CountryCode):
+    latest_version = tbcml.GameVersion.from_string_latest("latest", cc)
+    path = tbcml.Path("latest.txt")
     if not path.exists():
         path.create()
     lines = path.read().to_str().split("\n")
     if len(lines) < 4:
         lines = ["", "", "", ""]
     index = 0
-    if cc == core.CountryCode.EN:
+    if cc == tbcml.CountryCode.EN:
         index = 0
-    elif cc == core.CountryCode.JP:
+    elif cc == tbcml.CountryCode.JP:
         index = 1
-    elif cc == core.CountryCode.KR:
+    elif cc == tbcml.CountryCode.KR:
         index = 2
-    elif cc == core.CountryCode.TW:
+    elif cc == tbcml.CountryCode.TW:
         index = 3
     lines[index] = latest_version.to_string() + cc.get_code()
-    path.write(core.Data("\n".join(lines).encode("utf-8")))
+    path.write(tbcml.Data("\n".join(lines).encode("utf-8")))
 
 
 def update_all():
-    for cc in core.CountryCode.get_all():
+    for cc in tbcml.CountryCode.get_all():
         do(cc)
 
 
 if __name__ == "__main__":
     # update_all()
-    do(core.CountryCode.JP)
+    do(tbcml.CountryCode.EN)
