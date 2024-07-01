9dc44cc6c9081c5f6f458d5c2da9cfec9c986a15
gitbot <gitbot@henrythomas.me.uk>
2024-07-01 16:53:41 +0100

    improved efficiency

--- archive_upload.py
+++ archive_upload.py
@@ -29,6 +29,21 @@ def does_file_exist(identifier: str, filename: str) -> bool:
     response = requests.head(url, allow_redirects=True)
     return response.status_code == 200
 
+def get_all_files(identifier: str) -> list[str]:
+    url = f"https://archive.org/metadata/{identifier}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return
+
+    metadata = response.json()
+    files = metadata.get('files', [])
+    file_list: list[str] = []
+    for file in files:
+        file_list.append(file.get("name", ""))
+
+    return file_list
+
 
 dotenv.load_dotenv()
 
@@ -52,16 +67,18 @@ all_ccs = [
 
 def upload_apks(cc: tbcml.CountryCode):
     all_gvs = tbcml.Apk.get_all_versions(cc)
+    item_identifier = f"jp.co.ponos.battlecats{cc.get_patching_code()}"
+
+    all_files = get_all_files(item_identifier)
     for i, gv in enumerate(all_gvs[::-1]):
         log(f"{i+1}/{len(all_gvs)}")
         apk = tbcml.Apk(gv, cc)
         log(f"{cc.value, gv.to_string()}")
         apk_path = apk.pkg_path
-        item_identifier = f"jp.co.ponos.battlecats{cc.get_patching_code()}"
         api_url = f"https://s3.us.archive.org/{item_identifier}"
 
         file = f"{gv}.apk"
-        if does_file_exist(item_identifier, file):
+        if file in all_files:
             log(f"Skipping {file} {cc} as it already exists")
             continue
 
@@ -77,7 +94,7 @@ def upload_apks(cc: tbcml.CountryCode):
             log(f"Skipping {file} {cc} as it is not original")
             continue
 
-        upload_file(cc, apk_path, api_url, file)
+        upload_file(cc, apk_path, api_url, file, all_files)
 
     return all_gvs
 
@@ -105,23 +122,33 @@ def upload_server_files(latest_gv: tbcml.GameVersion, cc: tbcml.CountryCode):
             log(str(res.error))
             return
 
-        upload_directory(
-            cc,
-            apk.get_server_path(),
-            f"https://s3.us.archive.org/jp.co.ponos.battlecats{cc.get_patching_code()}",
-        )
+    upload_directory(
+        cc,
+        apk.get_server_path(),
+        f"jp.co.ponos.battlecats{cc.get_patching_code()}",
+    )
 
 
-def upload_directory(cc: tbcml.CountryCode, path: tbcml.Path, api_url: str):
+def upload_directory(cc: tbcml.CountryCode, path: tbcml.Path, identifier: str):
     log(f"Uploading {path} {cc}")
+
+    all_files = get_all_files(identifier)
+
 
     # Upload all the files in the directory
     for file in path.get_files():
-        upload_file(cc, file, api_url, file.get_file_name())
+        upload_file(cc, file, identifier, file.get_file_name(), all_files)
 
 
-def upload_file(cc: tbcml.CountryCode, path: tbcml.Path, api_url: str, file: str):
+def upload_file(cc: tbcml.CountryCode, path: tbcml.Path, identifier: str, file: str, all_files: list[str]):
     log(f"Uploading {file} {cc}")
+
+
+    if file in all_files or does_file_exist(identifier, file):
+        log(f"Already exists!")
+        return
+
+    api_url = f"https://s3.us.archive.org/{identifier}"
 
     # Open the file
     with open(path.to_str(), "rb") as f:
@@ -138,11 +165,15 @@ def upload_file(cc: tbcml.CountryCode, path: tbcml.Path, api_url: str, file: str
         # Create a monitor to track the progress of the upload
         monitor = MultipartEncoderMonitor(encoder, callback)
         # Upload the file
-        response = session.put(
-            f"{api_url}/{file}",
-            data=monitor,
-            headers={"Content-Type": monitor.content_type},
-        )
+        try:
+            response = session.put(
+                f"{api_url}/{file}",
+                data=monitor,
+                headers={"Content-Type": monitor.content_type},
+            )
+        except Exception as e:
+            print(e)
+            return
         # Print the response
 
         log(str(response.status_code))
